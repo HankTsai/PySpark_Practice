223cae567b0d25fe6f8a5fcd33d0369adb3e9f8c
Hank Tsai <HankTsai@users.noreply.github.com>
2020-11-28 13:53:26 +0800

    2020.11.28

---
+++ Streaming/user_tweets_producer.py
@@ -0,0 +1,39 @@
+from kafka import KafkaProducer
+import time
+import csv
+
+if __name__ == "__main__":
+    # Configurationss
+    rate = 100
+    topic = "tweets_stream"
+    bootstrap_servers = "localhost:9092"
+
+    # Create Kafka producer
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+
+    # open CSV file containing users' messages
+    with open("/home/spark/Desktop/spark_streaming_101/simple_sentiment_analysis_demo/data/messages.csv") as csvfile:
+        # initialize counters
+        batch_message_sent_count = 0
+        error_count = 0
+
+        # open CVS file
+        rows = csv.reader(csvfile)
+
+        # Send messages
+        while True:
+            
+            for row in rows:
+                try:
+                    user = row[4]
+                    message = row[5]
+                    producer.send(topic, key=bytes(user, "utf8"), value=bytes(message, "utf8"))
+                    print("hello")
+                except:
+                    error_count += 1
+
+                batch_message_sent_count += 1
+
+                if rate == batch_message_sent_count:
+                    batch_message_sent_count = 0
+                    time.sleep(1)
